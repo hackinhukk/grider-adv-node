a557d22012a8b417720d5097dd9181bb214e540d
Trevor Thomas <trevorthomas@mac.com>
2019-06-08 08:39:36 -0400

    ci test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -17,7 +17,7 @@ describe('When logged in', async () => {
     await page.login();
     await page.click('a.btn-floating');
   });
-
+// removalable comment
   test('can see blog create form', async () => {
     const label = await page.getContentsOf('form label');
 
